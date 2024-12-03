070c640d61a121b5701435bc2be2a95935b03f7e
nourcr0cs <nourimene2005@gmail.com>
2024-12-03 19:30:15 +0100

    update

--- src/app.py
+++ src/app.py
@@ -2,28 +2,26 @@ from flask import Flask, request, render_template
 import joblib
 import string
 import re
+import nltk
+nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-# Load the trained model and vectorizer
+# Loading
 model = joblib.load('spam_detection.joblib')
 tfidf_vectorizer = joblib.load('spam_count.joblib')
 stop_words = set(stopwords.words('english'))
 
 def preprocess_text(text):
-    # Remove punctuation
+    text = re.sub(r'http\S+|www\S+|bit\S+', 'URL', text)
     text = ''.join([char for char in text if char not in string.punctuation])
-    
-    # Remove numbers
     text = re.sub(r'\d+', '', text)
-    
-    # Remove stopwords
     text = " ".join([word for word in text.split() if word.lower() not in stop_words])
-    
-    # Tokenize
+    text = re.sub(r"([a-z])([A-Z])", r"\1 \2", text)
+    text = re.sub(r"(?<=[a-zA-Z])(?=\d)|(?<=\d)(?=[a-zA-Z])", " ", text)  
+    text = " ".join(re.findall(r"[A-Za-z]+|\d+", text)) 
     tokens = word_tokenize(text)
-    
-    return " ".join(tokens)  # Return the cleaned text as a string
+    return " ".join(tokens) 
 
 app = Flask(__name__)
 
@@ -33,24 +31,21 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    email_content = request.form['email']  # Get the email content from the form
+    email_content = request.form['email']  
     
-    # Preprocess the email content
     processed_email = preprocess_text(email_content)
-    
-    # Vectorize the email content using the pre-trained TF-IDF vectorizer
     email_tfidf = tfidf_vectorizer.transform([processed_email]).toarray()
     
-    # Make the prediction using the trained model
+   
     prediction = model.predict(email_tfidf)
     
-    # Return the result to the user
+  
     if prediction == 1:
         result = "This email is spam."
     else:
         result = "This email is not spam."
     
-    return render_template('result.html', result=result)  # Render the result page
+    return render_template('result.html', result=result)  
 
 if __name__ == "__main__":
     app.run(debug=True)
